617bcadb73e455e61376d64b08bd549cbb7ee3e0
sergeibogdaniuk <sergei.bogdaniuk@gmail.com>
2021-02-13 14:36:15 +0200

    refactor(utils): move sum function to utils.js

--- sum.js
+++
@@ -1,1 +0,0 @@
-export const sum = (...args) => new Array.from(args).reduce((acc, item) => acc + item)
--- utils.js
+++ utils.js
@@ -1,1 +1,3 @@
 export const divide = (a, b) => a / b
+
+export const sum = (...args) => new Array.from(args).reduce((acc, item) => acc + item)
